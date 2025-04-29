66030840b6c2fb1f886c3eef5bec43e6b6445584
João Vitor Cassiano <49155106+vitcas@users.noreply.github.com>
2025-04-29 16:44:10 -0300

    Update database.py

--- database.py
+++ database.py
@@ -9,4 +9,5 @@ HOST = os.getenv("HOST")
 DB_PORT = os.getenv("DB_PORT")
 DBNAME = os.getenv("DBNAME")
 
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{DB_PORT}/{DBNAME}?sslmode=require"
+#DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{DB_PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = f"postgresql://postgres.oludipbtxguatsdhgkba:{PASSWORD}@aws-0-sa-east-1.pooler.supabase.com:5432/postgres"
