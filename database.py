bb9909872ca1503e026fa0cfba78fce5e13a6226
João Vitor Cassiano <49155106+vitcas@users.noreply.github.com>
2025-04-29 15:39:36 -0300

    Update database.py

--- database.py
+++ database.py
@@ -9,4 +9,4 @@ HOST = os.getenv("HOST")
 DB_PORT = os.getenv("DB_PORT")
 DBNAME = os.getenv("DBNAME")
 
-DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{DB_PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg2:://{USER}:{PASSWORD}@{HOST}:{DB_PORT}/{DBNAME}?sslmode=require"
