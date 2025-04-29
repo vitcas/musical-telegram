baec68ff2260252e98acbabba48ec8beb953a387
João Vitor Cassiano <49155106+vitcas@users.noreply.github.com>
2025-04-29 01:54:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI, Response
-from fastapi.responses import PlainTextResponse
 import random
-import kai  # certifique-se de que kai.py está junto no repositório
+import kai
 
 app = FastAPI()
 
-@app.get("/", response_class=PlainTextResponse)
+@app.get("/generate-deck", response_class=Response)
 def generate_deck():
     mode = random.choice([1, 2, 3])
     content = kai.mode_select(mode)
-    return content
-
-# para Vercel funcionar
-# cria uma variável chamada "handler"
-from mangum import Mangum
-handler = Mangum(app)
+    return Response(content=content, media_type="text/plain")
