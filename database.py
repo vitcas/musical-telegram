2f2ef7c7ad881fa822218de0651860556694adda
João Vitor Cassiano <49155106+vitcas@users.noreply.github.com>
2025-04-29 14:12:39 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import os
 
 #load_dotenv()  # Carrega variáveis do arquivo .env
 
-USER = os.environ.get("USER")
-PASSWORD = os.environ.get("PASSWORD")
-HOST = os.environ.get("HOST")
-PORT = os.environ.get("PORT")
-DBNAME = os.environ.get("DBNAME")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DBNAME = os.getenv("DBNAME")
 
 DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
